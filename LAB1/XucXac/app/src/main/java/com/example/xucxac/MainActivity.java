c3089135dc7222982d263ecf306261a4b8a121e7
carefe27 <22a1001d0036@students.hou.edu.vn>
2024-08-31 01:17:55 +0700

    lab1_2

---
+++ LAB1/XucXac/app/src/main/java/com/example/xucxac/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.xucxac;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button rollDiceButton;
+    private ImageView diceImageView;
+    private Random random;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rollDiceButton = findViewById(R.id.rollDiceButton);
+        diceImageView = findViewById(R.id.diceImageView);
+        random = new Random();
+        rollDiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rollDice();
+            }
+        });
+    }
+    private void rollDice() {
+        int diceRoll = random.nextInt(6) + 1;
+        int drawableResource;
+        switch (diceRoll) {
+            case 1:
+                drawableResource = R.drawable.dice_1;
+                break;
+            case 2:
+                drawableResource = R.drawable.dice_2;
+                break;
+            case 3:
+                drawableResource = R.drawable.dice_3;
+                break;
+            case 4:
+                drawableResource = R.drawable.dice_4;
+                break;
+            case 5:
+                drawableResource = R.drawable.dice_5;
+                break;
+            case 6:
+                drawableResource = R.drawable.dice_6;
+                break;
+            default:
+                drawableResource = R.drawable.dice_1;
+        }
+        diceImageView.setImageResource(drawableResource);
+
+    }
+}
